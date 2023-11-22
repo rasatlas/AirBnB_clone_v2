ed46c6152ed666d2ce874aacf314925edc8c3837
Zemichael <yetikimitabeba@gmail.com>
2023-11-22 18:15:59 +0300

    pycodestyle edit

--- models/city.py
+++ models/city.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
