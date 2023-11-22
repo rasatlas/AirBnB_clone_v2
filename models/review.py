eb1680736ae51d46c0269bc3dfb6e0327a71c19a
Zemichael <yetikimitabeba@gmail.com>
2023-11-22 18:27:33 +0300

    pycodestyle fix

--- models/review.py
+++ models/review.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, ForeignKey
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = 'reviews'
-    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
