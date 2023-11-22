655050bee082fcfde49b982a39d54adeb921103a
Zemichael <yetikimitabeba@gmail.com>
2023-11-22 18:49:38 +0300

    Import file fix

--- models/state.py
+++ models/state.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+import os
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.engine.file_storage import FileStorage
-from models.city import City
-
 
 HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
