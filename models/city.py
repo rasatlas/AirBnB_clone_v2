6265afb6a16013887e5b86ea3867aa1e12618170
Ermi24et <lilermi24@gmail.com>
2023-11-23 01:26:19 +0300

    updating city to handle filestorage

--- models/city.py
+++ models/city.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='delete', backref='cities')
+    if HBNB_TYPE_STORAGE == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade='delete', backref='cities')
+    else:
+        name = ""
+        state_id = ""
