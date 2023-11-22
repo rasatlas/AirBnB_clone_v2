f8001e9b449e5c9bd2361463fc8ac6b9526676cc
Zemichael <yetikimitabeba@gmail.com>
2023-11-22 18:29:25 +0300

    pycodstyle fix

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
