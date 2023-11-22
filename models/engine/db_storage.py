0aa93075a45218a953736b02830c841f1fe4234d
Zemichael <yetikimitabeba@gmail.com>
2023-11-22 23:58:55 +0300

    comments added

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
 HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
-# HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
+# HBNB_MYSQL_HOST = localhost
 HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
 HBNB_ENV = os.getenv('HBNB_ENV')
 
