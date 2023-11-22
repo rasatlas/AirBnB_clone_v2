00afd4579c27282df324dd96376763f399642bff
Ermi24et <lilermi24@gmail.com>
2023-11-23 01:25:27 +0300

    updating review to handle filestorage

--- models/review.py
+++ models/review.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
+import os
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 
 
+HBNB_TYPE_STORAGE = os.getenv('HBNB_TYPE_STORAGE')
+
 class Review(BaseModel, Base):
     """ Review class to store review information """
     __tablename__ = 'reviews'
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if HBNB_TYPE_STORAGE == 'db':
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
