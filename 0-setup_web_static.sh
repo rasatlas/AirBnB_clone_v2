8d9582bf86bbce6538aa5b33a70803d035809cf7
Zemichael <yetikimitabeba@gmail.com>
2023-12-07 20:41:46 +0300

    Bash script that sets up servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,8 +22,25 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 
 printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By 316245-web-01;
+    root   /var/www/html;
+    index  index.html index.htm;
+
 	location /hbnb_static {
 		alias /data/web_static/current/
 	}
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 }" > /etc/nginx/sites-available/default
+
 sudo service nginx restart
