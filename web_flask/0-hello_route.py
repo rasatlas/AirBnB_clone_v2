6ff9c1165d5f42a0ed7f6410161f649aea56cea3
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-28 17:22:43 +0300

    Script that starts a flask webapp

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,4 +2,9 @@
 """Script that starts a Flask web application"""
 
 def hello():
+    """
+    The web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+    """
     return "Hello HBNB!"
