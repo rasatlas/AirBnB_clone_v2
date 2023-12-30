ce9e0973afb9b398cd65f2570f57f9655b9aaf62
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2023-12-30 17:11:11 +0300

    Script that list states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -21,11 +21,13 @@ def list_states():
     - Routes:
         - /states_list: display a HTML page: (inside the tag BODY)
         - H1 tag: “States”
-        - UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+        - UL tag: with the list of all State objects present in DBStorage
+        sorted by name (A->Z) tip
         - LI tag: description of one State: <state.id>: <B><state.name></B>
     """
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
