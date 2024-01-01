db53429b87391b970aaa07ce54007532262725ed
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-01 22:00:35 +0300

    Script that starts flask webapp

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,18 +9,18 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def list_states(id=None):
+@app.route('/states/<state_id>', strict_slashes=False)
+def list_states(state_id=''):
     """
     Display HTML page with the states sorted alphabetically
     If state with a matching id as the passed id is found, list
     all cities linked to the state with same id.
     If no matching id is found display: Not found!
     """
-    states = storage.all(state.State).values()
+    states = storage.all(state.State)
     if id is not None:
-        id = state.State + id
-    return render_template('9-states.html', states=states, id=id)
+        state_id = 'state.State' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
 
 
 @app.teardown_appcontext
