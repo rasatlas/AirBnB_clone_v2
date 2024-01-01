d587d1d839d1e18ecf605d59f60c2b29d6bd6b70
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-01 17:49:40 +0300

    sorting edit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,40 +1,25 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""
+Script that starts a Flask web application
+"""
 
 from models import *
-from flask import Flask
-from flask import render_template
+from models import storage
+from flask import Flask, render_template
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
-def list_states():
-    """
-    - Script that lists states
-    - You must use storage for fetching data from the storage engine
-    (FileStorage or DBStorage) from models import storage
-    and storage.all(...)
-    After each request you must remove the current SQLAlchemy Session:
-    - Declare a method to handle @app.teardown_appcontext
-    - Call in this method storage.close()
-    - Routes:
-        - /states_list: display a HTML page: (inside the tag BODY)
-        - H1 tag: “States”
-        - UL tag: with the list of all State objects present in DBStorage
-        sorted by name (A->Z) tip
-        - LI tag: description of one State: <state.id>: <B><state.name></B>
-    """
-    states = storage.all('State').values()
-    sorted_states = sorted(tuple(states), key=lambda s: s.name)
-    return render_template('7-states_list.html', states=sorted_states)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception=None):
-    """Closes the storage on teardown"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
