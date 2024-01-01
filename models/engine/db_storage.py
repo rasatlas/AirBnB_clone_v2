24178aa3ab9a4597e4342f0bcacaa5bb87c12823
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-01 16:18:44 +0300

    Session variable creation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -87,7 +87,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        self.__session = scoped_session(session_factory)()
+        Session = scoped_session(session_factory)()
+        self.__session = Session
 
     def close(self):
         """ closes the session """
