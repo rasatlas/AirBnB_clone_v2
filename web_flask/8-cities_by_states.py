e9d376f22dca1c28112d48fe626e2d59e6f1269e
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-01 21:00:24 +0300

    Script that lists states and cities alphabetically

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from models import storage, state, city
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_list():
+    """
+    Display HTML page with the states and their corresponding cities
+    listed in alphabetical order
+    """
+    states = storage.all(state.State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
