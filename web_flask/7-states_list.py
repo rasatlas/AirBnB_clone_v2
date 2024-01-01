981ca00b7a2ff99c00a242d0ef28387c461a04d1
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-01 18:56:17 +0300

    sorting fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,7 +4,6 @@ Script that starts a Flask web application
 """
 
 from models import *
-from models import storage
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -12,8 +11,11 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    # Retrieve the State objects using the State model
+    states = storage.all('State').all()
+    # Sort the states by name
+    states_sorted = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.py', states=states_sorted)
 
 
 @app.teardown_appcontext
