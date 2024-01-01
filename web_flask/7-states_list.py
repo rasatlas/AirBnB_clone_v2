208f45e5d7f5f2261c062630ec560cd27c508c1c
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-01 17:45:09 +0300

    typo edit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -26,7 +26,7 @@ def list_states():
         - LI tag: description of one State: <state.id>: <B><state.name></B>
     """
     states = storage.all('State').values()
-    sorted_states = sorted(list(states), key=lambda s: s.name)
+    sorted_states = sorted(tuple(states), key=lambda s: s.name)
     return render_template('7-states_list.html', states=sorted_states)
 
 
