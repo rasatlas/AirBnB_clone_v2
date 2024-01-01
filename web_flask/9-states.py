bc70b572f807c9f4e7c1f3ada0172c3695707fbd
Zemichael T. Yadeta <yetikimitabeba@gmail.com>
2024-01-01 21:39:08 +0300

    Script that starts flask webapp

---
+++ web_flask/9-states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+
+from models import storage, state
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def list_states(id=None):
+    """
+    Display HTML page with the states sorted alphabetically
+    If state with a matching id as the passed id is found, list
+    all cities linked to the state with same id.
+    If no matching id is found display: Not found!
+    """
+    states = storage.all(state.State).values()
+    if id is not None:
+        id = state.State + id
+    return render_template('9-states.html', states=states, id=id)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
